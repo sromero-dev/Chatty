29216a1ff32341da747b34f36b2c9889079c85f6
sromero-dev <sromerodiaz.work@gmail.com>
2025-11-25 14:53:49 +0100

    message model

---
+++ backend/src/model/message.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const messageScheme = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    recieverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageScheme);
+
+export default Message;
