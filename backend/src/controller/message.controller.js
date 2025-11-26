c067190176f35d0ba11cd48ff5fcd254280d8d86
sromero-dev <sromerodiaz.work@gmail.com>
2025-11-26 09:05:12 +0100

    backend code done

---
+++ backend/src/controller/message.controller.js
@@ -0,0 +1,67 @@
+import User from "../model/user.model.js";
+import Message from "../model/message.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      // Find all users except the logged in user
+      _id: { $ne: loggedInUserId },
+    }).select("-password"); // Select password from decoded cookied user
+
+    res.status(200).json(filteredUsers); // Returns filtered users
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error: " + error });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    // Extract user data
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, recieverId: userToChatId },
+        { senderId: userToChatId, recieverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error: " + error });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: recieverId } = req.params;
+
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to Cloudinary
+      const uploadResponde = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponde.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId: req.user._id,
+      recieverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //TODO: socket.io
+
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error: " + error });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json()); //TODO En el readme marcar la importancia del orden
 app.use(cookieParser()); // Allows you to parse cookie (grab value out of them)
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
